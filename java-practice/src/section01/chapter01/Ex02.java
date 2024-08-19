4f183320a24abf2e047dede0b3d684dea1372a0b
yong_ho.kim <yong_ho.kim@kt.com>
2024-08-19 15:50:31 +0900

    240819 - 학습

--- java-practice/src/section01/chapter01/Ex02.java
+++ java-practice/src/section01/chapter01/Ex02.java
@@ -3,5 +3,11 @@ package section01.chapter01;
 public class Ex02 {
     public static void main(String[] args) {
 
+        byte byteNum;
+        int smallIntNum = 123;
+
+        //  명시적(강제) 형변환
+        //  - 개발자 : "내가 책임질테니까 그냥 넣으세요."
+        byteNum = (byte)smallIntNum;
     }
 }
--- java-practice/src/section01/chapter02/Ex01.java
+++ java-practice/src/section01/chapter02/Ex01.java
@@ -3,5 +3,10 @@ package section01.chapter02;
 public class Ex01 {
     public static void main(String[] args) {
 
+        double a = 0.1, b = 0.2;
+
+        //  ⚠️ 오차가 생김
+        double c = a + b;
+
     }
 }
